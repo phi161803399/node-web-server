b4796e4eb3f688e06e91802740646be1ffaf95ec
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2017-11-30 10:51:48 +0100

    testing express applications II

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,9 +3,29 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res
+    .status(200)
+    .send([{
+        name: 'Guus',
+        age: 31
+      }, {
+        name: 'Mark',
+        age: 40
+      }, {
+        name: 'Anselm',
+        age: 33
+      }]);
 });
 
 app.listen(3000, () => {
   console.log('server is up on port 3000');
 });
+
+module.exports.app = app;
